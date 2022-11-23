26edfde0864861f4e1b4409d115865410d605111
Tomsoya8810 <soyan8810@gmail.com>
2022-11-23 12:51:29 +0900

    コミット忘れ

--- src/views/DatabaseView.vue
+++ src/views/DatabaseView.vue
@@ -10,20 +10,6 @@
 import appBarVue from "@/components/AppBar.vue";
 import sideBarVue from "@/components/SideBar.vue";
 export default {
-  data() {
-    return {
-      data: [],
-    };
-  },
-
-  methods: {
-    test() {
-      console.log(this.data);
-    },
-  },
-  created() {
-    this.data = this.$route.params.data;
-  },
   components: { appBarVue, sideBarVue },
 };
 </script>
